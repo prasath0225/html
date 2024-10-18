83c38b718b4bc1e1587879cb1cc0fd66cb2776bb
prasath0225 <divyaprasath055@gmail.com>
2024-10-18 18:31:37 +0530

    Add files via upload

---
+++ class7.py
@@ -0,0 +1,19 @@
+#output
+name=input("enter your name")
+age=input("age")
+schoolname=input("school")
+
+#input
+english=int(input("enter your english value"))
+tamil=int(input("enter your tamil value"))
+maths=int(input("enter your maths value"))
+science=int(input("enter your science value"))
+social=int(input("enter your social value"))
+
+#typecase
+totalmarks=("Marks=english+tamil+maths+science+social")
+print("totalmarks")
+
+
+
+
---
+++ main3.java
@@ -0,0 +1,23 @@
+class main3 extends son
+{
+public static void main(String args)
+{
+Scanner scan=new Scanner(System.in);
+
+System.out.println("enter the first number:");
+int a=scan.nextInt();
+System.out.println("enter the second number:"):
+int b=scan.nextInt();
+
+son obj=new son();
+int addition = obj.add();
+int subraction = obj.sub();
+int multiplication = obj.mul();
+int divition = obj.div();
+
+System.out.println("addition:"+add);
+System.out.println("subraction:"+sub);
+System.out.println("multiplication:"+mul);
+System.out.println("divition:"+div);
+}
+}
